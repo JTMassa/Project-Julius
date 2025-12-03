4154edaffc930433cb1399b13af72945eafc30f1
JTMassa <tawejulius1975@gmail.com>
2025-12-03 12:42:23 -0500

    gitignore/output add

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "my_vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+output "public_subnet1" {
+  value = aws_subnet.my_public_subnet1.id
+}
+output "public_subnet2" {
+  value = aws_subnet.my_public_subnet2.id
+}
+output "private_subnet1" {
+  value = aws_subnet.my_private_subnet1.id
+}
+output "private_subnet2" {
+  value = aws_subnet.my_private_subnet2.id
+}
